84a4c35f90e390699b1000dd156104867a6052d8
carlosrodriguezgarcia <al315679@uji.es>
2019-04-03 16:10:15 +0200

    DAO, Controller y vistas de contrato

---
+++ src/main/java/es/uji/ei1050/ccc/controller/ContratoController.java
@@ -0,0 +1,172 @@
+package es.uji.ei1050.ccc.controller;
+
+import es.uji.ei1050.ccc.model.Contrato;
+import es.uji.ei1050.ccc.model.Perfiles;
+import es.uji.ei1050.ccc.model.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+
+import es.uji.ei1050.ccc.daos.ContratoDAO;
+
+
+@Controller
+@RequestMapping("/contrato")
+public class ContratoController {
+
+    private ContratoDAO contratoDao;
+
+    @Autowired
+    public void setTrabajdoroDao(ContratoDAO contratoDao) {
+        this.contratoDao = contratoDao;
+    }
+
+
+    /**
+     * Vista para ver los datos del contrato de un trabajador.
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/informacion", method = RequestMethod.GET)
+    public String verInformacionTrabajador(HttpSession session, Model model) {
+        if (session.getAttribute("usuario") == null)
+        {
+            model.addAttribute("usuario", new Usuario());
+            return "login";
+        }
+
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        Perfiles tipo = user.getTipo();
+        if(tipo.equals(Perfiles.JF.getDescripcion()) || tipo.equals(Perfiles.TR.getDescripcion())) {
+            String dni = (String) session.getAttribute("DNI");
+            model.addAttribute("contrato", contratoDao.getContrato(dni));
+            return "contrato/informacion";
+        } else {
+            model.addAttribute("error", "No tienes permiso para acceder a este sitio");
+            return "redirect:" + session.getAttribute("url");
+        }
+    }
+
+    //Métodos añadir un contrato
+
+    /**
+     * Vista para añadir una trabajador.
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/añadir")
+    public String añadirTrabajador(HttpSession session, Model model) {
+        if (session.getAttribute("usuario") == null)
+        {
+            model.addAttribute("usuario", new Usuario());
+            return "login";
+        }
+
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        Perfiles tipo = user.getTipo();
+        String dni = (String) session.getAttribute("DNI");
+
+        if(tipo.equals(Perfiles.JF.getDescripcion())) {
+            Contrato contrato = new Contrato();
+            contrato.setPersone_dni(dni);
+            model.addAttribute("contrato", contrato);
+            return "contrato/añadir";
+        } else {
+            model.addAttribute("error", "No tienes permiso para acceder a este sitio");
+            return "redirect:/" + session.getAttribute("url");
+        }
+    }
+
+    /**
+     * Método que añade el trabajador.
+     * @param contrato
+     * @param bindingResult
+     * @return
+     */
+    @RequestMapping(value = "/añadir", method = RequestMethod.POST)
+    public String processAddSubmit(@ModelAttribute("contrato") Contrato contrato,
+                                   BindingResult bindingResult) {
+
+        //AlumnoValidator alumnoValidator = new AlumnoValidator();
+        //alumnoValidator.validate(alumno, bindingResult);
+
+        if (bindingResult.hasErrors())
+            return "contrato/añadir";
+        contratoDao.addContrato(contrato);
+        return "redirect:list.html :: list";
+    }
+
+    /**
+     * Método que borra un contrato
+     * @param dni
+     * @return
+     */
+    @RequestMapping(value = "/borrar/{dni}", method = RequestMethod.DELETE)
+    public String processDelete(@PathVariable String dni) {
+        contratoDao.deleteContrato(dni);
+        return "redirect:../list";
+    }
+
+    //Métodos editar trabajador
+
+    /**
+     * Vista para actualizar los datos de un trabajador.
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping(value = "/editar/{dni}", method = RequestMethod.GET)
+    public String updateTrabajador(HttpSession session, Model model) {
+        if (session.getAttribute("usuario") == null)
+        {
+            model.addAttribute("usuario", new Usuario());
+            return "login";
+        }
+
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        Perfiles tipo = user.getTipo();
+        if(tipo.equals(Perfiles.JF.getDescripcion())) {
+            String dni = (String) session.getAttribute("DNI");
+            model.addAttribute("contrato", contratoDao.getContrato(dni));
+            return "contrato/editar";
+        } else {
+            model.addAttribute("error", "No tienes permiso para acceder a este sitio");
+            return "redirect:" + session.getAttribute("url");
+        }
+    }
+
+    /**
+     * Método que actualiza los datos del trabajador.
+     * @param session
+
+     * @param contrato
+     * @param bindingResult
+     * @return
+     */
+    @RequestMapping(value = "/editar", method = RequestMethod.POST)
+    public String processUpdateSubmit(@RequestBody String data, HttpSession session,
+                                      @ModelAttribute("contrato") Contrato contrato,
+                                      BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "contrato/editar";
+
+
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        String dni = (String) session.getAttribute("DNI");
+        contrato = contratoDao.getContrato(dni);
+        Perfiles tipo = user.getTipo();
+
+        if(tipo.equals(Perfiles.JF.getDescripcion()) || tipo.equals(Perfiles.TR.getDescripcion())) {
+            contratoDao.updateContrato(contrato);
+            return "contrato/informacion";
+        } else {
+            return "redirect:" + session.getAttribute("url");
+        }
+    }
+}
---
+++ src/main/java/es/uji/ei1050/ccc/daos/ContratoDAO.java
@@ -0,0 +1,122 @@
+package es.uji.ei1050.ccc.daos;
+
+import es.uji.ei1050.ccc.model.Contrato;
+import es.uji.ei1050.ccc.model.ETipoContrato;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository("contrato")
+public class ContratoDAO {
+
+
+    /**
+     * <code>SpringFramework</code> object used to access databases.
+     */
+    private JdbcTemplate jdbcTemplate;
+
+    /**
+     * Method that initializes the <code>JdbcTemplate</code> with the database source.
+     * @param dataSource Database source used to initialize the <code>JdbcTemplate</code>.
+     */
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    /**
+     * Inner class that maps database objects to <code>Alumno</code> objects.
+     */
+    private static final class ContratoMapper implements RowMapper<Contrato> {
+
+        /**
+         * Method that maps database objects to <code>Alumno</code> objects.
+         * @param rs Database result.
+         * @param rowNum Number of rows in Database result <code>rs</code>.
+         * @return An <code>Alumno</code> object.
+         * @throws SQLException If something goes wrong.
+         */
+        public Contrato mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Contrato contrato = new Contrato();
+            contrato.setSueldoBase(rs.getInt("sueldoBase"));
+            contrato.setDiasVacaciones(rs.getInt("diasVacaciones"));
+            contrato.setTipoContrato(ETipoContrato.getEstado(rs.getString("tipoContrato")));
+            contrato.setPersone_dni(rs.getString("Persone_dni"));
+            return contrato;
+        }
+    }
+
+    //Método para ver el sueldo base de un trabajador
+
+    public Contrato getSueldoBase(String dni) {
+        try {
+            return this.jdbcTemplate.queryForObject(
+                    "select  p.nombre, p.apellidos, c.sueldoBase" +
+                            "from persone p join contrato c on (p.dni = c.Persone_dni) where upper(dni)=?",
+                    new Object[]{dni}, new ContratoMapper());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Método para ver los dias de vacaciones restantes de un trabajador
+
+    public Contrato getDiasVacaciones(String dni) {
+        try {
+            return this.jdbcTemplate.queryForObject(
+                    "select  p.nombre, p.apellidos, c.diasVacaciones" +
+                            "from persone p join contrato c on (p.dni = c.Persone_dni) where upper(dni)=?",
+                    new Object[]{dni}, new ContratoMapper());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Método para ver el contrato de un trabajado
+
+    public Contrato getContrato(String dni) {
+        try {
+            return this.jdbcTemplate.queryForObject(
+                    "select  p.nombre, p.apellidos, c.sueldoBase, c.diasVacaciones, c.tipoContrato" +
+                            "from persone p join contrato c on (p.dni = c.Persone_dni) where upper(dni)=?",
+                    new Object[]{dni}, new ContratoMapper());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    //Método para añadir un contrato
+
+    public void addContrato(Contrato contrato) {
+        if(this.jdbcTemplate.update(
+                "insert into contrato(sueldoBase, diasVacaciones, tipoContrato, Persone_dni) values(?, ?, ?, ?)",
+                contrato.getSueldoBase(), contrato.getDiasVacaciones(), contrato.getTipoContrato(), contrato.getPersone_dni()) > 0 );
+    }
+
+
+    //Método para editar un contrato
+
+    public void updateContrato(Contrato contrato) {
+        if(this.jdbcTemplate.update(
+                "update contrato set sueldoBase=?, diasVacaciones=?, tipoContrato=? where upper(Persone_dni) = ?",
+                contrato.getSueldoBase(), contrato.getDiasVacaciones(), contrato.getTipoContrato(), contrato.getPersone_dni() ) > 0);
+    }
+
+    //Método para borrar un contrato
+
+    public boolean deleteContrato(String dni) {
+        if(this.jdbcTemplate.update("delete from contrato where upper(Persone_dni) = ?", dni) > 0)
+           return true;
+        return false;
+    }
+
+
+}

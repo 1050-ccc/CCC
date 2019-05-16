048245ee1b692e05e8ab36b9c4e94c3ba34f76df
carlosrodriguezgarcia <al315679@uji.es>
2019-05-16 17:06:45 +0200

    El jefe puede consultar las horas que ha trabajado un trabajador durante el mes actual

--- src/main/java/es/uji/ei1050/ccc/controller/HorarioController.java
+++ src/main/java/es/uji/ei1050/ccc/controller/HorarioController.java
@@ -6,6 +6,7 @@ import es.uji.ei1050.ccc.model.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
@@ -55,6 +56,58 @@ public class HorarioController {
         }
     }
 
+    /**
+     * Lista horas trabajadas.
+     * @param session
+     * @param model
+     * @return
+     */
+    @RequestMapping("/listahoras/{dni}")
+    public String listaHorasTrabajadasTrabajador(HttpSession session, Model model, @PathVariable String dni) {
+        if (session.getAttribute("usuario") == null)
+        {
+            model.addAttribute("usuario", new Usuario());
+            return "login";
+        }
 
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        Perfiles tipo = user.getTipo();
+        //String dni = (String) session.getAttribute("DNI");
+
+        Calendar fecha = Calendar.getInstance();
+        int mes = fecha.get(Calendar.MONTH);
+        int año = fecha.get(Calendar.YEAR);
+
+
+        if(tipo.getDescripcion().equals(Perfiles.JF.getDescripcion())) {
+            model.addAttribute("horarios", horarioDao.getHorasTrabajadas(dni, (mes + 1), año));
+            return "horario/listahoras";
+
+        } else {
+            model.addAttribute("error", "No tienes permiso para acceder a este sitio");
+            return "redirect:/trabajador";
+        }
+    }
+
+    @RequestMapping("/consulta")
+    public String consultaHorasTrabajadas(HttpSession session, Model model) {
+        if (session.getAttribute("usuario") == null)
+        {
+            model.addAttribute("usuario", new Usuario());
+            return "login";
+        }
+
+        Usuario user = (Usuario) session.getAttribute("usuario");
+        Perfiles tipo = user.getTipo();
+
+
+        if(tipo.getDescripcion().equals(Perfiles.TR.getDescripcion())) {
+            return "horario/consulta";
+
+        } else {
+            model.addAttribute("error", "No tienes permiso para acceder a este sitio");
+            return "redirect:/trabajador";
+        }
+    }
 
 }
